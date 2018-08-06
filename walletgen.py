9bdf6cf19c38e87827ae728466f880fe687dd87a
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2018-08-06 18:57:28 +0200

    generate root key from seed

--- walletgen.py
+++ walletgen.py
@@ -1,5 +1,11 @@
 from mnemonic import Mnemonic
-
+from bip32utils import BIP32Key
 m = Mnemonic('english')
 words = m.generate(strength=256)
 print(words)
+seed = m.to_seed(words)
+print("************")
+print("Seed " + seed.hex())
+key = BIP32Key.fromEntropy(seed)
+xprv = BIP32Key.fromEntropy(seed).ExtendedKey()
+print("Root Key is " + xprv)
