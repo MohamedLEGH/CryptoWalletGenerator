d0542efe4e5f658179d6976b6c83c584c049acd8
MohamedLEGH <mohamed.amine.legheraba@gmail.com>
2018-08-06 19:06:07 +0200

    computation of a bitcoin address

--- walletgen.py
+++ walletgen.py
@@ -6,6 +6,17 @@ print(words)
 seed = m.to_seed(words)
 print("************")
 print("Seed " + seed.hex())
-key = BIP32Key.fromEntropy(seed)
+#key = BIP32Key.fromEntropy(seed)
+
 xprv = BIP32Key.fromEntropy(seed).ExtendedKey()
 print("Root Key is " + xprv)
+
+key = BIP32Key.fromExtendedKey(xprv)
+from bip32utils import BIP32_HARDEN
+
+print(key.ChildKey(44 + BIP32_HARDEN) \
+     .ChildKey(0 + BIP32_HARDEN) \
+     .ChildKey(0 + BIP32_HARDEN) \
+     .ChildKey(0) \
+     .ChildKey(0) \
+.Address())
